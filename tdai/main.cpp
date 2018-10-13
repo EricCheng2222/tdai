607eeae10cc581cedaf34da0f0715f1d48c25f35
Eric <ericjang@kimo.com>
2018-10-13 22:09:20 +0800

    Initial Commit

---
+++ tdai/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tdai
+//
+//  Created by Eric Cheng on 10/13/18.
+//  Copyright © 2018 Eric Cheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
